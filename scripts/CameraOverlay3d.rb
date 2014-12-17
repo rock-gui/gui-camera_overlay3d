3146678b58d53a16390668e4f30a07f3a181af32
Malte Wirkus <malte.wirkus@dfki.de>
2014-12-17 11:18:55 +0100

    Integrated transformer script into CameraOvetrlay.rb

--- scripts/CameraOverlay3d.rb
+++ scripts/CameraOverlay3d.rb
@@ -1,28 +1,86 @@
 require 'vizkit'
+require 'transformer/runtime'
+require 'pry'
+
 urdf_file = "#{ENV["AILA_BUNDLE_DIR"]}/data/aila.urdf"
+limits = "#{ENV["AILA_BUNDLE_DIR"]}/config/joint_limits/joint_limits_bodywithhead.yml"
+
+Orocos.load_typekit('robot_frames')
+
+#############################################################
 
 image_files = Dir.entries('images').map do |f| f if f.split('.')[-1]=='png' end.compact!.reverse!
 
-overlay = Vizkit.default_loader.CameraOverlay3D
-vis_gui = Vizkit.default_loader.RobotVisualization
-vis_gui.modelFile = urdf_file
-vis_gui.jointsSize = 0.02
+#Setup chains we want to publish
+chains=[]
+c=Types::RobotFrames::Chain.new
+c.root_link = "base_link"
+c.name = "cam"
+c.tip_link = "LeftCamera"
+chains.push c.dup
 
-viz.frame = "LeftCamera"
-viz.resetCamera()
+c.name = "left_arm"
+c.tip_link = "Wrist_l"
+chains.push c.dup
 
-t=Qt::Timer.new
-t.start(5)
+c.name = "right_arm"
+c.tip_link = "Wrist_r"
+chains.push c.dup
 
-t.connect(t, SIGNAL('timeout()')) do 
-   current_image = image_files.pop
-   if not current_image
-       t.stop
-   end
-
-   overlay.updateImageFromFile(current_image)
+#Set up transformer config
+chains.each do |c|
+    Orocos.transformer.manager.conf.dynamic_transform "fk.#{c.name}",c.root_link => c.tip_link
 end
 
+#############################################################
 
+overlay = Vizkit.default_loader.CameraOverlay3D
+roboviz = Vizkit.default_loader.RobotVisualization
+roboviz.modelFile = urdf_file
+roboviz.jointsSize = 0.02
+ctrl_gui=Vizkit.default_loader.ControlUi
+ctrl_gui.initFromYaml(limits)
 
-Vizkit.exec
+overlay.frame = "LeftCamera"
+overlay.resetCamera()
+
+Orocos.run Transformer.broadcaster_name, 'robot_frames::ChainPublisher' => "fk" do
+  begin
+    #Change images every 5 seconds
+    t=Qt::Timer.new
+    t.start(5)
+    idx=0
+    t.connect(t, SIGNAL('timeout()')) do
+       current_image = image_files[idx]
+       overlay.updateImageFromFile(current_image)
+       idx = (idx+1) % image_files.size
+    end
+
+    #Make robot controllable via gui. Update FK and visualization
+    fk = Orocos.name_service.get 'fk'
+    fk.chains = chains
+    fk.urdf_file = urdf_file
+    fk.configure
+    fk.start
+    port_writer = fk.input.writer
+
+    ctrl_gui.enableSendCBs(true)
+
+    ctrl_gui.connect(SIGNAL('sendSignal()')) do
+        port_writer.write(ctrl_gui.getJoints())
+        roboviz.updateData(ctrl_gui.getJoints())
+    end
+
+    sleep(5)
+
+    #Orocos.transformer.update_configuration_state
+    #bc=Orocos.name_service.get(Transformer.broadcaster_name)
+    #bc.start
+    #sleep(1)
+    #bc.setConfiguration Orocos.transformer.configuration_state
+    Vizkit.exec
+  rescue Exception => ex
+    puts ex
+  end
+end
+
